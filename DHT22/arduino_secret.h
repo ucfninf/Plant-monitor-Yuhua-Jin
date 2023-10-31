e23ce69af12232e985fef6b4f035c391ae0a07f9
ucfninf <146268411+ucfninf@users.noreply.github.com>
2023-10-31 17:24:22 +0000

    Update arduino_secret.h

--- DHT22/arduino_secret.h
+++ DHT22/arduino_secret.h
@@ -1,4 +1,4 @@
 #define SECRET_SSID "CE-Hub-Student"
-#define SECRET_PASS "casa-ce-gagarin-public-service"
+#define SECRET_PASS ""
 #define SECRET_MQTTUSER "student"
-#define SECRET_MQTTPASS "ce2021-mqtt-forget-whale";
+#define SECRET_MQTTPASS "";
