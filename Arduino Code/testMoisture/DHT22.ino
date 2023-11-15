0ce989245fc8dc58390b803b788d88b6ca79d306
Yuhua (Annie）Jin <146268411+ucfninf@users.noreply.github.com>
2023-11-15 22:57:35 +0000

    Customize SSID and passwords

--- Arduino Code/testMoisture/DHT22.ino
+++ Arduino Code/testMoisture/DHT22.ino
@@ -1,12 +1,3 @@
-/*
-    Get date and time - uses the ezTime library at https://github.com/ropg/ezTime -
-    and then show data from a DHT22 on a web page served by the Huzzah and
-    push data to an MQTT server - uses library from https://pubsubclient.knolleary.net
-
-    Duncan Wilson
-    CASA0014 - 2 - Plant Monitor Workshop
-    May 2020
-*/
 
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
